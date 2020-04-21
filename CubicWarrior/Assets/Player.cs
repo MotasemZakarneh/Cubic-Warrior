fa9c285690dc2b3b9c56590cbc4fd1b9398d22ab
Motasem Zakarneh <mutrz99@gmail.com>
2020-04-21 07:53:23 +0300

    Initial commit

---
+++ CubicWarrior/Assets/Player.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Material[] mats = new Material[0];
+}
